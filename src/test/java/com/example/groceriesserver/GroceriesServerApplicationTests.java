54eff522eb9bd1957fb300a1345be70ae28881da
Vijay Naik <vnaik@broadsoft.com>
2020-05-23 16:44:50 -0400

    Initial Commit

---
+++ src/test/java/com/example/groceriesserver/GroceriesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groceriesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
